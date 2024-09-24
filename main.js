5872d3b5f12ad3b41f54de81add41e9073dc598c
Muhammad Talha <118286576+MuhammadTalha011@users.noreply.github.com>
2024-09-24 19:50:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text" , {
+    strings:["Front-end Developer" , "Wordpress Developer" , "Java Apps Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
