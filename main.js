485bc7d20f9a6454d7b8bebdafdd5e5c71c325a4
Muhammad Talha <118286576+MuhammadTalha011@users.noreply.github.com>
2024-10-02 01:43:29 +0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".text" , {
-    strings:["Front-end Developer" , "Wordpress Developer" , "Java Apps Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-
-});
